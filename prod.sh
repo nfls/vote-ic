6e4ecc7c16221f293c0bcdd900a9fd9f24817d3e
Qingyang Hu <huqingyang2000@gmail.com>
2019-07-26 00:59:10 +0800

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -1,9 +1,6 @@
 git submodule update --init --recursive --remote
 npm install
 composer install --ignore-platform-reqs
-cd public/assets
-bower install --allow-root
-cd ../..
 ./node_modules/.bin/encore production
 bin/console doctrine:schema:update --force
 bin/console cache:clear
