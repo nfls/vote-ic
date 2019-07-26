54b84732d8452b74d27e2fd6ec7bd63d84ae76cd
Qingyang Hu <huqingyang2000@gmail.com>
2019-07-26 21:02:24 +0800

    Clear cache on deploy

--- prod.sh
+++ prod.sh
@@ -4,4 +4,7 @@ composer install --ignore-platform-reqs
 ./node_modules/.bin/encore production
 bin/console doctrine:schema:update --force
 bin/console cache:clear
+bin/console doctrine:cache:clear-result
+bin/console doctrine:cache:clear-metadata
+bin/console doctrine:cache:clear-query
 bin/console cache:warmup --env=prod
