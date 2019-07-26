9c8076e7c5fb87c0fb0781ca4c3175b4b4ce680a
Qingyang Hu <huqingyang2000@gmail.com>
2019-07-27 01:24:29 +0800

    Update webpack

--- prod.sh
+++ prod.sh
@@ -1,6 +1,7 @@
 git submodule update --init --recursive --remote
 npm install
 composer install --ignore-platform-reqs
+rm -rf node_modules/.cache/babel-loader/
 ./node_modules/.bin/encore production
 bin/console doctrine:schema:update --force
 bin/console cache:clear
